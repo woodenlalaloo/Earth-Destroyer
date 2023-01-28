187824ca51afc5a56eb75a143fda3aa4616e1889
Woodenlalaloo <56805723+woodenlalaloo@users.noreply.github.com>
2023-01-28 00:03:50 +0000

    game frame

--- main.cs
+++ main.cs
@@ -1,7 +1,36 @@
 using System;
+using System.Timers;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+	public static long WorldPopulation = 8000000000;
+	private static System.Timers.Timer aTimer;
+   
+   public static void Main()
+   {
+      SetTimer();
+
+      Console.WriteLine("\nPress the Enter key to exit the application...\n");
+      Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+      Console.ReadLine();
+      aTimer.Stop();
+      aTimer.Dispose();
+      
+      Console.WriteLine("Terminating the application...");
+   }
+
+   private static void SetTimer()
+   {
+        // Create a timer with a two second interval.
+        aTimer = new System.Timers.Timer(2000);
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+		WorldPopulation = WorldPopulation + 10000000;
+        Console.WriteLine(WorldPopulation);
+    }
 }
